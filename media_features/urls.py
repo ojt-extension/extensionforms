755249e63905a249edebfe8f186d669672804830
Louie <christianlouiecorpuz@gmail.com>
2025-08-18 10:28:19 +0800

    Added urls

--- media_features/urls.py
+++ media_features/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     path('table-9-student-involvement/', views.student_involvement_form, name='table_9_form'),
     path('table-10-media-features/', views.media_feature_form, name='table_10_form'),
     path('table-11-technologies/', views.technology_commercialized_form, name='table_11_form'),
+    path('table-12-ordinance/', views.ordinance_form, name='table_12_form'),
+    path('table-13-impact-assessment/', views.impact_assessment_form, name='table_13_form'),
+    path('table-14-awards/', views.extension_ppa_form, name='table_14_form'),
+    path('table-15-other-activities/', views.student_extension_involvementm, name='table_15_form'),
 
     # New: This is the centralized URL for the multi-sheet Excel export
     path('export-all/', views.export_all_to_excel, name='export_all_to_excel'),
